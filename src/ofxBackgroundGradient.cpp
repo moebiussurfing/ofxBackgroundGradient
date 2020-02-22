3d17becf74281f3c02fb3d7126510f9ad3a366f6
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-02-22 06:52:43 -0300

    +

--- src/ofxBackgroundGradient.cpp
+++ src/ofxBackgroundGradient.cpp
@@ -8,72 +8,72 @@ ofxBackgroundGradient::ofxBackgroundGradient()
 //--------------------------------------------------------------
 ofxBackgroundGradient::~ofxBackgroundGradient()
 {
-    ofRemoveListener(params.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
-    ofRemoveListener(params_controls.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
+	ofRemoveListener(params.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
+	ofRemoveListener(params_controls.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
 
-    if (autoSaveLoad)
-        XML_save(params, path_folder + path_file);
+	if (autoSaveLoad)
+		XML_save(params, path_folder + path_file);
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::setup()
 {
-    pos.set("offset", glm::vec2(0, 0),
-        glm::vec2(-ofGetWidth(), -ofGetHeight()),
-        glm::vec2(ofGetWidth(), ofGetHeight()));
-    color1.set("Color Gradient 1", ofColor(127), ofColor(0, 0), ofColor(255));
-    color2.set("Color Gradient 2", ofColor(127), ofColor(0, 0), ofColor(255));
-    gradientType.set("type", 1, 0, 2);
-    gradientType_str.set("TYPE:", "");
-    scaleX.set("scaleX", 2.0f, 0.1f, 5.0f);
-    scaleY.set("scaleY", 2.0f, 0.1f, 5.0f);
+	pos.set("offset", glm::vec2(0, 0),
+		glm::vec2(-ofGetWidth(), -ofGetHeight()),
+		glm::vec2(ofGetWidth(), ofGetHeight()));
+	color1.set("Color Gradient 1", ofColor(127), ofColor(0, 0), ofColor(255));
+	color2.set("Color Gradient 2", ofColor(127), ofColor(0, 0), ofColor(255));
+	gradientType.set("type", 1, 0, 2);
+	gradientType_str.set("", "");
+	scaleX.set("scaleX", 2.0f, 0.1f, 5.0f);
+	scaleY.set("scaleY", 2.0f, 0.1f, 5.0f);
 
-    bScaleLink.set("Link Scales", true);
-    degrees.set("degrees", 0.0f, 0.0f, 360.0f);
-    bRandomize.set("Randomize All", false);
-    bRandomizeColors.set("Randomize Colors", false);
-    bResetAll.set("Reset All", false);
-    bResetTransform.set("Reset Transform", false);
+	bScaleLink.set("Link Scales", true);
+	degrees.set("degrees", 0.0f, 0.0f, 360.0f);
+	bRandomize.set("Randomize All", false);
+	bRandomizeColors.set("Randomize Colors", false);
+	bResetAll.set("Reset All", false);
+	bResetTransform.set("Reset Transform", false);
 
-    params.setName("GRADIENT BACKGROUND");
-    params.add(color1);
-    params.add(color2);
-    params.add(gradientType);
-    params.add(gradientType_str);
-    gradientType_str.setSerializable(false);
+	params.setName("GRADIENT BACKGROUND");
+	params.add(color1);
+	params.add(color2);
+	params.add(gradientType);
+	params.add(gradientType_str);
+	gradientType_str.setSerializable(false);
 
-    params_circleMode.setName("CIRCULAR TYPE");
-    params_circleMode.add(pos);
-    params_circleMode.add(scaleX);
-    params_circleMode.add(scaleY);
-    //params_circleMode.add(degrees);
-    params.add(params_circleMode);
+	params_circleMode.setName("CIRCULAR TYPE");
+	params_circleMode.add(pos);
+	params_circleMode.add(scaleX);
+	params_circleMode.add(scaleY);
+	//params_circleMode.add(degrees);
+	params.add(params_circleMode);
 
-    params_controls.setName("CONTROLS");
+	params_controls.setName("CONTROLS");
 
-    bScaleLink.setSerializable(false);
-    bRandomize.setSerializable(false);
-    bRandomizeColors.setSerializable(false);
-    bRandomizeColors.setSerializable(false);
-    bResetTransform.setSerializable(false);
-    bResetAll.setSerializable(false);
+	bScaleLink.setSerializable(false);
+	bRandomize.setSerializable(false);
+	bRandomizeColors.setSerializable(false);
+	bRandomizeColors.setSerializable(false);
+	bResetTransform.setSerializable(false);
+	bResetAll.setSerializable(false);
 
-    params_controls.add(bScaleLink);
-    params_controls.add(bRandomize);
-    params_controls.add(bRandomizeColors);
-    params_controls.add(bResetTransform);
-    params_controls.add(bResetAll);
-    params.add(params_controls);
+	params_controls.add(bScaleLink);
+	params_controls.add(bRandomize);
+	params_controls.add(bRandomizeColors);
+	params_controls.add(bResetTransform);
+	params_controls.add(bResetAll);
+	params.add(params_controls);
 
-    gui.setup();
-    gui.add(params);
-    //gui.add(params_controls);
+	gui.setup();
+	gui.add(params);
+	//gui.add(params_controls);
 
-    ofAddListener(params.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
-    ofAddListener(params_controls.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
+	ofAddListener(params.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
+	ofAddListener(params_controls.parameterChangedE(), this, &ofxBackgroundGradient::Changed_Params);
 
-    if (autoSaveLoad)
-        XML_load(params, path_folder + path_file);
+	if (autoSaveLoad)
+		XML_load(params, path_folder + path_file);
 
 	//collapse
 	auto &g0 = gui.getGroup("GRADIENT BACKGROUND");
@@ -89,147 +89,149 @@ void ofxBackgroundGradient::setup()
 //--------------------------------------------------------------
 void ofxBackgroundGradient::drawBackground()
 {
-    //only in circular gradient mode
-    if (gradientType == 1)
-    {
-        ofPushMatrix();
-        ofTranslate(pos.get().x, pos.get().y);
-        ofScale(scaleX, scaleY);
-        ofBackground(color2);
-    }
+	//only in circular gradient mode
+	if (gradientType == 1)
+	{
+		ofPushMatrix();
+		ofTranslate(pos.get().x, pos.get().y);
+		ofScale(scaleX, scaleY);
+		ofBackground(color2);
+	}
 
-    ofBackgroundGradient(color1, color2, ofGradientMode(gradientType.get()));
+	ofBackgroundGradient(color1, color2, ofGradientMode(gradientType.get()));
 
-    //only in circular gradient mode
-    if (gradientType == 1)
-    {
-        ofPopMatrix();
-    }
+	//only in circular gradient mode
+	if (gradientType == 1)
+	{
+		ofPopMatrix();
+	}
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::drawGui()
 {
-    gui.draw();
+	gui.draw();
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::draw()
 {
-    drawBackground();
-    drawGui();
+	drawBackground();
+	drawGui();
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::randomize()
 {
-    color1.set(randomColor());
-    color2.set(randomColor());
-    gradientType = (int) ofRandom(0.0f, 3.0f);
-    scaleX = (int) ofRandom(0.5f, 6.0f);
-    scaleY = (int) ofRandom(0.5f, 6.0f);
+	color1.set(randomColor());
+	color2.set(randomColor());
+	gradientType = (int)ofRandom(0.0f, 3.0f);
+	scaleX = (int)ofRandom(0.5f, 6.0f);
+	scaleY = (int)ofRandom(0.5f, 6.0f);
 }
 
 //------------------------------------------------------------
 ofColor ofxBackgroundGradient::randomColor()
 {
-    ofColor myColor;
-    myColor.set(ofRandom(255), ofRandom(255), ofRandom(255));
-    return myColor;
+	ofColor myColor;
+	myColor.set(ofRandom(255), ofRandom(255), ofRandom(255));
+	return myColor;
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::resetAll()
 {
-    color1.set(ofColor(127));
-    color2.set(ofColor(24));
-    gradientType = 0;
-    resetTransform();
+	color1.set(ofColor(127));
+	color2.set(ofColor(24));
+	gradientType = 0;
+	resetTransform();
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::resetTransform()
 {
-    pos = glm::vec2(0, 0);
-    scaleX = scaleY = 1.0;
-    degrees = 0;
+	pos = glm::vec2(0, 0);
+	scaleX = scaleY = 1.0;
+	degrees = 0;
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::Changed_Params(ofAbstractParameter &e)
 {
-    string name = e.getName();
+	string name = e.getName();
 
-    if (name == "Randomize All")
-    {
-        if (bRandomize)
-        {
-            bRandomize = false;
-            randomize();
-        }
-    }
-    else if (name == "Randomize Colors")
-    {
-        if (bRandomizeColors)
-        {
-            bRandomizeColors = false;
-            color1.set(randomColor());
-            color2.set(randomColor());
-        }
-    }
-    else if (name == "Reset All")
-    {
-        if (bResetAll)
-        {
-            bResetAll = false;
-            resetAll();
-        }
-    }
-    else if (name == "Reset Transform")
-    {
-        if (bResetTransform)
-        {
-            bResetTransform = false;
-            resetTransform();
-        }
-    }
-    else if (name == "Link Scales")
-    {
-        if (bScaleLink)
-        {
-            scaleY = scaleX;
-        }
-    }
-    else if (name == "scaleX")
-    {
-        if (bScaleLink)
-        {
-            scaleY = scaleX;
-        }
-    }
-    else if (name == "scaleY")
-    {
-        if (bScaleLink)
-        {
-            scaleX = scaleY;
-        }
-    }
-    else if (name == "type")
-    {
-        switch (gradientType)
-        {
-            case 0:
-                gradientType_str = "LINEAR";
-                break;
-            case 1:
-                gradientType_str = "CIRCULAR";
-				break;
-            case 2:
-                gradientType_str = "BAR";
-                break;
+	if (name == "Randomize All")
+	{
+		if (bRandomize)
+		{
+			bRandomize = false;
+			randomize();
 		}
+	}
+	else if (name == "Randomize Colors")
+	{
+		if (bRandomizeColors)
+		{
+			bRandomizeColors = false;
+			color1.set(randomColor());
+			color2.set(randomColor());
+		}
+	}
+	else if (name == "Reset All")
+	{
+		if (bResetAll)
+		{
+			bResetAll = false;
+			resetAll();
+		}
+	}
+	else if (name == "Reset Transform")
+	{
+		if (bResetTransform)
+		{
+			bResetTransform = false;
+			resetTransform();
+		}
+	}
+	else if (name == "Link Scales")
+	{
+		if (bScaleLink)
+		{
+			scaleY = scaleX;
+		}
+	}
+	else if (name == "scaleX")
+	{
+		if (bScaleLink)
+		{
+			scaleY = scaleX;
+		}
+	}
+	else if (name == "scaleY")
+	{
+		if (bScaleLink)
+		{
+			scaleX = scaleY;
+		}
+	}
+	else if (name == "type")
+	{
+		switch (gradientType)
+		{
+		case 0:
+			gradientType_str = "LINEAR";
+			break;
+		case 1:
+			gradientType_str = "CIRCULAR";
+			break;
+		case 2:
+			gradientType_str = "BAR";
+			break;
+		}
+
+		typeChanged = true;
 		refreshGui();
-    }
+	}
 }
 
 //--------------------------------------------------------------
@@ -260,7 +262,7 @@ void ofxBackgroundGradient::refreshGuiPtr(ofxPanel &_gui)
 //}
 
 //--------------------------------------------------------------
-void ofxBackgroundGradient::refreshGui() 
+void ofxBackgroundGradient::refreshGui()
 {
 	//collapse
 	auto &g0 = gui.getGroup("GRADIENT BACKGROUND");
@@ -275,22 +277,22 @@ void ofxBackgroundGradient::refreshGui()
 //--------------------------------------------------------------
 void ofxBackgroundGradient::XML_load(ofParameterGroup &g, string path)
 {
-    ofLogNotice("ofxBackgroundGradient") << "XML_load " << path;
-    ofXml settings;
+	ofLogNotice("ofxBackgroundGradient") << "XML_load " << path;
+	ofXml settings;
 
-    bool b = settings.load(path);
-    if (b)
-        ofDeserialize(settings, g);
-    else
-        ofLogError("ofxBackgroundGradient") << "FILE NOT FOUND!";
+	bool b = settings.load(path);
+	if (b)
+		ofDeserialize(settings, g);
+	else
+		ofLogError("ofxBackgroundGradient") << "FILE NOT FOUND!";
 }
 
 //--------------------------------------------------------------
 void ofxBackgroundGradient::XML_save(ofParameterGroup &g, string path)
 {
-    ofLogNotice("ofxBackgroundGradient") << "XML_save " << path;
+	ofLogNotice("ofxBackgroundGradient") << "XML_save " << path;
 
-    ofXml settings;
-    ofSerialize(settings, g);
-    settings.save(path);
+	ofXml settings;
+	ofSerialize(settings, g);
+	settings.save(path);
 }
--- src/ofxBackgroundGradient.h
+++ src/ofxBackgroundGradient.h
@@ -7,74 +7,95 @@ class ofxBackgroundGradient
 {
 public:
 
-    ofxBackgroundGradient();
-    ~ofxBackgroundGradient();
+	ofxBackgroundGradient();
+	~ofxBackgroundGradient();
 
-    void setup();
+	void setup();
 
-    void drawBackground();
-    void drawGui();
-    void draw();//both
+	void drawBackground();
+	void drawGui();
+	void draw();//both
 
-    //void update();
-    //void exit();
+	//void update();
+	//void exit();
 
-    ofParameterGroup params;//the required paramaters to store preset/states
-    ofParameterGroup params_controls;//we need to show the controls outside too
-    ofParameter<ofColor> color1;
-    ofParameter<ofColor> color2;
+	ofParameterGroup params;//the required paramaters to store preset/states
+	ofParameterGroup params_controls;//we need to show the controls outside too
+	ofParameter<ofColor> color1;
+	ofParameter<ofColor> color2;
 
-    ofxPanel gui;
+	ofxPanel gui;
 
-    void setAutoSaveLoad(bool b)
-    {
-        autoSaveLoad = b;
-        ofLogNotice("ofxBackgroundGradient") << "setAutoSaveLoad: " << b;
-    }
+	void setAutoSaveLoad(bool b)
+	{
+		autoSaveLoad = b;
+		ofLogNotice("ofxBackgroundGradient") << "setAutoSaveLoad: " << b;
+	}
 
 	void refreshGui();
-	
+
 	//TODO:
 	//this method could handle the collapsing of the groups
 	//but should use guiGroups...or to pass the gui pannel and the parent levels..
 	void refreshGuiPtr(ofxPanel &_gui);
 	//void refreshGuiGrp(ofParameterGroup &_group);
 
-	void reset() 
+	void reset()
 	{
 		resetAll();
 	}
-    
+
+	//to update gui outside addon class
+	bool isTypeChanged()
+	{
+		if (typeChanged)
+		{
+			typeChanged = false;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	int getType()
+	{
+		return gradientType.get();
+	}
+
 	//-
 
 private:
 
-    ofParameterGroup params_circleMode;
+	//to update gui outside addon class
+	bool typeChanged = false;
 
-    ofParameter<glm::vec2> pos;
-    ofParameter<float> scaleX;//for circle mode only
-    ofParameter<float> scaleY;//for circle mode only
-    ofParameter<int> gradientType;
-    ofParameter<string> gradientType_str;
-    // TODO: this requires a custom gradient/mesh drawing method..
-    ofParameter<float> degrees;
-    ofParameter<bool> bRandomize;
-    ofParameter<bool> bRandomizeColors;
-    ofParameter<bool> bResetAll;
-    ofParameter<bool> bResetTransform;
-    ofParameter<bool> bScaleLink;
-    void Changed_Params(ofAbstractParameter &e);
+	ofParameterGroup params_circleMode;
 
-    ofColor randomColor();
-    void randomize();
-    void resetAll();
-    void resetTransform();
+	ofParameter<glm::vec2> pos;
+	ofParameter<float> scaleX;//for circle mode only
+	ofParameter<float> scaleY;//for circle mode only
+	ofParameter<int> gradientType;
+	ofParameter<string> gradientType_str;
+	// TODO: this requires a custom gradient/mesh drawing method..
+	ofParameter<float> degrees;
+	ofParameter<bool> bRandomize;
+	ofParameter<bool> bRandomizeColors;
+	ofParameter<bool> bResetAll;
+	ofParameter<bool> bResetTransform;
+	ofParameter<bool> bScaleLink;
+	void Changed_Params(ofAbstractParameter &e);
 
-    bool autoSaveLoad = false;
-    string path_folder = "ofxBackgroundGradient/";
-    string path_file = "myBackground.xml";
-    void XML_load(ofParameterGroup &g, string path);
-    void XML_save(ofParameterGroup &g, string path);
+	ofColor randomColor();
+	void randomize();
+	void resetAll();
+	void resetTransform();
+
+	bool autoSaveLoad = false;
+	string path_folder = "ofxBackgroundGradient/";
+	string path_file = "myBackground.xml";
+	void XML_load(ofParameterGroup &g, string path);
+	void XML_save(ofParameterGroup &g, string path);
 };
 
 
